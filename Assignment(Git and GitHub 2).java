7320e204a068e72362871398601da78d611ea102
Ranjitkumar-pce <159939192+Ranjitkumar-pce@users.noreply.github.com>
2024-02-15 21:14:03 +0530

    Add files via upload

---
+++ Assignment(Git and GitHub 2).java
@@ -0,0 +1,25 @@
+Assignment Questions
+1. How to check if git is available on your system?
+Ans. By using git --version command.
+2. How to initialize a new Git repository?
+Ans. By using git init command.
+3.How to tell git about your name and email?
+Ans. Command for name – git config --global user.name “user name”
+         Command for email – git config --global user.email “user email”
+4.How to add a file to the staging area?
+Ans. Command for one file- git add filename
+         Command for multiple files at once – git add .
+5. How to remove a file from the staging area?
+Ans. By using git rm --cached file name
+6.How to make a commit?
+Ans. git commit -m “Assignment commit”
+7.How to send your changes to a remote repository?
+Ans. git push
+8. What is the difference between clone and pull?
+Ans. Clone: -By using git clone command, we can download the copy of a existing git repository to our local computer.
+         Pull: -By using git push command, we can download the copy of a modified git repository to our local computer.
+ 
+
+
+
+
