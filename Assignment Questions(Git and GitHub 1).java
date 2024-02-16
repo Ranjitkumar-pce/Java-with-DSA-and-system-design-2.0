b7c8e39f2c47abfc5ffec79315069115bd7c274c
Ranjitkumar-pce <159939192+Ranjitkumar-pce@users.noreply.github.com>
2024-02-16 20:00:56 +0530

    Add files via upload
    Git and GitHub 1

---
+++ Assignment Questions(Git and GitHub 1).java
@@ -0,0 +1,21 @@
+Assignment Questions
+1. What is Git?
+Ans. Git is a distributed version control system (DVCS) that is widely used in software development for tracking changes in source code during the development process. It was created by Linus Torvalds in 2005.
+2.What do you understand by the term ‘Version Control System’?
+Ans. It is a system that records changes to a file or set of files over time, so that we can recall specific version later, i.e., for every source code change in file a new version will be created.
+3.What is GitHub?
+Ans. It is a repository.
+It is the largest host of source code in the world.
+It is used to store or maintain the source code of the project.
+4. Mention some popular Git hosting services?
+Ans. GitHub, Gitlab, Bitbucket, Gitblit.
+5.Different types of version Control systems?
+Ans. There are two types of version Control Systems are :-
+Centralised Version control system (CVCS) have a single repository, from which each user receives their working copy.
+Distributed version control system (DVCS) contains multiple repositories, and different users can access each one with their working copy.
+6.What benefits come with using GIT?
+Ans. Open source, tracking code changes, coding collaborations, Branching, and merging.
+7.What is a Git repository?
+Ans. It is a location where all Git files are kept. Either the local repository or the remote repository may be used to store these files.
+8.How can you initialize a repository in Git?
+Ans. By using git init command in Git Bash.
